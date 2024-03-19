7634e6f136eacb1d2aeff91c70b6ba36b6ded456
Ivan G <mr.zero@inbox.ru>
2024-03-19 19:55:43 +0300

    fix: calculate discounts proportional to 5

--- src/utils/getDiscount.js
+++ src/utils/getDiscount.js
@@ -1,5 +1,5 @@
 const getDiscount = (discountPrice, regularPrice) => {
-  return Math.round((regularPrice - discountPrice) * 100 /regularPrice)
+  return Math.round((regularPrice - discountPrice) * 100 /(regularPrice * 5)) * 5
 }
 
 export default getDiscount
